fc2e8bcd831b2d3947f7173971a5119293177d3b
Tungk20WKS2 <nguyenhatung19795@gmail.com>
2025-07-08 14:02:11 +0700

    conditional out put

--- my-react-app/src/views/Example/ChiComponent.jsx
+++ my-react-app/src/views/Example/ChiComponent.jsx
@@ -1,31 +1,63 @@
 import React from "react";
 
-// class ChiComponent extends React.Component {
+class ChiComponent extends React.Component {
+    state = {
+        showjob : false
+    }
 
-//   render() {
-//     console.log("check props", this.props) 
+    HandleShow = (showjob) => {
+        this.setState({
+            showjob : true
+        })
+    }
 
-//     let {name, age, adress, arrJob } = this.props
-//     return (
-//       <div>
-//           {/* child component name : {name}     
-//           child component age : {age}
-//           child component adres : {adress} */}
-//           <h1> Job list</h1>
-//           {
-//             arrJob.map((item, index) => {
-//                 return (
-//                     <div key = {item.id}>
-//                         {item.title} - {item.salary}
-//                     </div>
-//                 )
-//             })
-//           }
+    HandleClose = (showjob) =>{
+        this.setState({
+            showjob: false
+        }) 
+    }
 
-//       </div>
-//     );
-//   }
-// }
+  render() {
+
+    console.log("check props", this.props) 
+
+    let {arrJob } = this.props
+    
+    let {showjob} = this.state
+
+    let check = showjob === true ? 'showjob = true' : 'showjob = false'
+
+    console.log("check", check)
+    return (
+        <React.Fragment>
+            {showjob === false ?
+            <div>
+                <button onClick = {(showjob) => this.HandleShow(showjob)}>Show</button>
+            </div>
+            :
+            <>
+                <div className = "job-list">
+                    <h1> Job list</h1>
+                    {
+                        arrJob.map((item, index) => {
+                            return (
+                                <div key = {item.id}>
+                                    {item.title} - {item.salary}
+                                </div>
+                            )
+                        })
+                    }
+
+                </div>
+                    <div>
+                        <button onClick={(showjob) => this.HandleClose(showjob)}>Hide</button>
+                    </div>
+                </>    
+            }
+        </React.Fragment>
+    );
+  }
+}
 
 
 
@@ -33,27 +65,30 @@ import React from "react";
 
 //function component
 //
-const ChiComponent = (props) => {
-    console.log("check props",props )
-    let {arrJob} = props; 
-    return (
-       <div>
-           {/* child component name : {name}     
-           child component age : {age}
-           child component adres : {adress} */}
-           <h1> Job list</h1>           {
-             arrJob.map((item, index) => {
-                 return (
-                     <div key = {item.id}>
-                         {item.title} - {item.salary}
-                     </div>
-                 )
-             })
-           }
+// const ChiComponent = (props) => {
+//     console.log("check props",props )
+//     let {arrJob} = props; 
+//     return (
+//        <div>
+//            {/* child component name : {name}     
+//            child component age : {age}
+//            child component adres : {adress} */}
+//            <h1> Job list</h1>           {
+//              arrJob.map((item, index) => {
+//                 if (+item.salary> 500)
+//                 {
+//                     return (
+//                         <div key = {item.id}>
+//                             {item.title} - {item.salary} $
+//                         </div>
+//                     )
+//                 }
+//              })
+//            }
 
-       </div>
-    )
-} 
+//        </div>
+//     )
+// } 
 
 
 export default ChiComponent;
