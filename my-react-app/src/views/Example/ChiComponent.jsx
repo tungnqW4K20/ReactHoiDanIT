d8a124c50a608c2f018c5a50746d4afa375a5aa6
Tungk20WKS2 <nguyenhatung19795@gmail.com>
2025-07-08 11:17:44 +0700

    arrow function + class function (extra)

--- my-react-app/src/views/Example/ChiComponent.jsx
+++ my-react-app/src/views/Example/ChiComponent.jsx
@@ -1,56 +1,59 @@
 import React from "react";
 
-class ChiComponent extends React.Component {
-state = {
-    firstname: '',
-    lastname: ''
-}
+// class ChiComponent extends React.Component {
 
-HandleOnChangeFirstname = (event) =>{
-    this.setState({
-        firstname : event.target.value
-    })
-}
+//   render() {
+//     console.log("check props", this.props) 
 
-HandleOnChangeLastname = (event) =>{
-    this.setState({
-        lastname : event.target.value
-    })
-}
+//     let {name, age, adress, arrJob } = this.props
+//     return (
+//       <div>
+//           {/* child component name : {name}     
+//           child component age : {age}
+//           child component adres : {adress} */}
+//           <h1> Job list</h1>
+//           {
+//             arrJob.map((item, index) => {
+//                 return (
+//                     <div key = {item.id}>
+//                         {item.title} - {item.salary}
+//                     </div>
+//                 )
+//             })
+//           }
 
-HandleOnClick = (event) =>{
-    event.preventDefault();
-    alert("đã submit thành công!")
-    console.log("Check data input", this.state)
-    this.setState({
-        firstname : '',
-        lastname :''
-    })
-}
+//       </div>
+//     );
+//   }
+// }
 
-  render() {
-    console.log("check props", this.props) 
 
-    let {name, age, adress, arrJob } = this.props
+
+
+
+//function component
+//
+const ChiComponent = (props) => {
+    console.log("check props",props )
+    let {arrJob} = props; 
     return (
-      <div>
-          {/* child component name : {name}     
-          child component age : {age}
-          child component adres : {adress} */}
-          <h1> Job list</h1>
-          {
-            arrJob.map((item, index) => {
-                return (
-                    <div key = {item.id}>
-                        {item.title} - {item.salary}
-                    </div>
-                )
-            })
-          }
+       <div>
+           {/* child component name : {name}     
+           child component age : {age}
+           child component adres : {adress} */}
+           <h1> Job list</h1>           {
+             arrJob.map((item, index) => {
+                 return (
+                     <div key = {item.id}>
+                         {item.title} - {item.salary}
+                     </div>
+                 )
+             })
+           }
 
-      </div>
-    );
-  }
-}
+       </div>
+    )
+} 
+
 
 export default ChiComponent;
